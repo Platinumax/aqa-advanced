38b845f9607efb300c62f88f000ed213be6ae65d
Vlasenko Maxim <platinumax5@gmail.com>
2025-03-30 21:22:35 +0300

    added Lesson4 homework

---
+++ JS/jsBasics/task1.js
@@ -0,0 +1,11 @@
+let stringVariable = 'String variable text';
+let numberVariable = 55;
+let boolVariable = true;
+let nullVariable = null;
+let undefinedVariable;
+
+console.log(stringVariable);
+console.log(numberVariable);
+console.log(boolVariable);
+console.log(nullVariable);
+console.log(undefinedVariable);
