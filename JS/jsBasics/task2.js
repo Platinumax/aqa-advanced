ae22abe9dcbf5f90740757f451ff506d36e9da61
Vlasenko Maxim <platinumax5@gmail.com>
2025-03-30 21:47:45 +0300

    added lesson4 homework

---
+++ JS/jsBasics/task2.js
@@ -0,0 +1,8 @@
+const persone1 = 'Василь';
+const persone2 = 'Марія';
+
+const greetingConcat = "Добрий день, " + persone1 + " та " + persone2 + "! Як ваші справи?";
+const greetingInterpolation = `Добрий день, ${persone1} та ${persone2}! Як ваші справи?`;
+
+console.log(greetingConcat);
+console.log(greetingInterpolation);
---
+++ JS/jsBasics/task4_3.js
@@ -0,0 +1,6 @@
+let radius = 7;
+let height = 6;
+
+const cylinderVolume = Math.PI * Math.pow(radius, 2) * height;
+
+console.log(`Об’єм циліндра з радіусом - ${radius} і висотою - ${height} дорівнює:`, cylinderVolume.toFixed(2));
